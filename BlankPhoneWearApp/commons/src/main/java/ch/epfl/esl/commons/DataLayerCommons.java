176e13289d1d6034ab710a3f2e012846b41820b4
Claudio Loureiro <claudio.loureiro1@gmail.com>
2018-01-15 14:25:09 +0000

    Add files via upload

--- BlankPhoneWearApp/commons/src/main/java/ch/epfl/esl/commons/DataLayerCommons.java
+++ BlankPhoneWearApp/commons/src/main/java/ch/epfl/esl/commons/DataLayerCommons.java
@@ -3,10 +3,12 @@ package ch.epfl.esl.commons;
 public class DataLayerCommons {
     public static final String START_ACTIVITY_PATH = "/start-activity";
     public static final String DATA_ITEM_RECEIVED_PATH = "/data-item-received";
-    public static final String COUNT_PATH = "/count";
     public static final String IMAGE_PATH = "/image";
-    public static final String IMAGE_KEY = "photo";
+    public static final String IMAGE_KEY = "image";
+    public static final String COUNT_PATH = "/count";
     public static final String COUNT_KEY = "count";
-    public static final String NOTIF_PATH = "/notif";
-    public static final String NOTIF_KEY = "notif";
+    public static final String NOTIFICATION_PATH = "/notification";
+    public static final String NOTIFICATION_KEY = "content";
+
+    public static final String ACTION_DISMISS = "ch.epfl.esl.DISMISS";
 }
