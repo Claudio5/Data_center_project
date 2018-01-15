d43163884deb99bb55c36fb4abbc7d0921f0a41e
Claudio <claudio.loureiro95@hotmail.com>
2018-01-15 13:23:10 +0100

    latest CL7

---
+++ BlankPhoneWearApp/Application/src/main/java/ch/epfl/esl/blankphonewearapp/MyIntentService.java
@@ -0,0 +1,8 @@
+package ch.epfl.esl.blankphonewearapp;
+
+/**
+ * Created by Claudio on 15.01.2018.
+ */
+
+public class MyIntentService {
+}
---
+++ BlankPhoneWearApp/Application/src/main/java/ch/epfl/esl/blankphonewearapp/TimePickerFragment.java
@@ -0,0 +1,8 @@
+package ch.epfl.esl.blankphonewearapp;
+
+/**
+ * Created by Claudio on 13.01.2018.
+ */
+
+public class TimePickerFragment {
+}
