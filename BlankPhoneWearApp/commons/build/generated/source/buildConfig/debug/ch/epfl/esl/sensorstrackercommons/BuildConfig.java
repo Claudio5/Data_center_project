8d95ec5bb157e0e3a503ae618d15f5485b7c8622
Claudio5 <claudio.loureiro1@gmail.com>
2020-09-07 14:51:52 +0200

    add gitignore and remove useless files

--- BlankPhoneWearApp/commons/build/generated/source/buildConfig/debug/ch/epfl/esl/sensorstrackercommons/BuildConfig.java
+++ BlankPhoneWearApp/commons/build/generated/source/buildConfig/debug/ch/epfl/esl/sensorstrackercommons/BuildConfig.java
@@ -5,9 +5,8 @@ package ch.epfl.esl.sensorstrackercommons;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "ch.epfl.esl.sensorstrackercommons";
+  public static final String LIBRARY_PACKAGE_NAME = "ch.epfl.esl.sensorstrackercommons";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
 }
